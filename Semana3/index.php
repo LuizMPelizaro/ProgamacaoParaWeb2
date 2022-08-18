be826d6755a9641e726c60a879bf194955888584
Luiz <luiz.pelizaro127@hotmail.com>
2022-08-18 15:16:51 -0300

    Create select method

--- Semana3/index.php
+++ Semana3/index.php
@@ -18,6 +18,29 @@
 
 <?php
 
+class TableRows extends RecursiveIteratorIterator
+{
+    function __construct($it)
+    {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current()
+    {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current() . "</td>";
+    }
+
+    function beginChildren()
+    {
+        echo "<tr>";
+    }
+
+    function endChildren()
+    {
+        echo "</tr>" . "\n";
+    }
+}
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -40,6 +63,20 @@ try {
     echo $sql . "<br>" . $exception->getMessage();
 }
 
+try {
+    $stmt = $conn->prepare("SELECT * FROM posts");
+    $stmt->execute();
+
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $key => $value) {
+        echo $value;
+        echo "<br>";
+    }
+} catch (PDOException $exception) {
+    echo "Error: " . $exception->getMessage();
+}
+$conn = null;
 ?>
 </body>
 </html>
